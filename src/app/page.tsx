4f1732e88a76d433aac36106dd3e20e196478242
Marat Dospolov <marat.dospolov@gmail.com>
2026-01-11 19:13:28 +0100

    images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default async function Index() {
           slug={heroPost.slug}
           excerpt={heroPost.excerpt}
         />
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        {/* {morePosts.length > 0 && <MoreStories posts={morePosts} />} */}
       </Container>
     </main>
   )
